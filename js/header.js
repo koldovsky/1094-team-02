702f0e25c4d73c0b2be3222a92383e953c390a46
Anton Lazarenko <129626897+SynapsError@users.noreply.github.com>
2023-11-19 17:10:47 +0000

    improved burger menu

--- js/header.js
+++ js/header.js
@@ -5,10 +5,10 @@ const navLinks = document.querySelector(".header__nav-menu");
 
 btnSub.addEventListener("click", () => {
     body.classList.toggle("fixed");
-    header.classList.toggle("responsive");
+    header.classList.toggle("active");
 })
 
 navLinks.addEventListener("click", () => {
     body.classList.remove("fixed");
-    header.classList.remove("responsive");
+    header.classList.remove("active");
 })
