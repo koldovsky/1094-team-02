63a664ebf476326696003bc948d5d3e3d034ef61
Miroslav-student <147755633+Miroslav-student@users.noreply.github.com>
2023-11-20 11:59:46 +0200

    Update tasks-02.js

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -1,1 +1,9 @@
-
+// Task Opposites Attract 
+function lovefunc(flower1, flower2) {
+  if (flower1 % 2 === 0 && flower2 % 2 != 0) {
+    return true;
+  } else if (flower2 % 2 === 0 && flower1 % 2 != 0) {
+    return true;
+  }
+  return false;
+}
