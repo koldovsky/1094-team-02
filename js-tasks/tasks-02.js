0e6d3ca24e9475321ac09f198d04f003046e1745
ivnedilko <ivnedilko@gmail.com>
2023-11-22 18:13:43 +0200

    Remove

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -11,14 +11,23 @@ function DNAtoRNA(dna) {
 }
 
 //https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
-var min = function (list) {
-  list.sort(function (a, b) { return a - b; });
-  return list[0];
+var min = function(list){
+  let result = list[0]; 
+  for(let i=0; i<list.length; i++){
+    if(list[i] < result){
+      result = list[i];
+    }
+  }
+  return result;
 }
-
-var max = function (list) {
-  list.sort(function (a, b) { return b - a; });
-  return list[0];
+var max = function(list){
+  let result = list[0];
+  for(let i=0; i<list.length; i++){
+    if(list[i] > result){
+      result = list[i];
+    }
+  }
+  return result;
 }
 
 //https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
