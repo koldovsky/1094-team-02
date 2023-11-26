c3bbf4c7675af9a5666b8db8609c86753ded886e
Maksym Derkach <136246093+AlphanumericUser01@users.noreply.github.com>
2023-11-26 16:02:59 +0000

    add js link

--- js/index.js
+++ js/index.js
@@ -2,4 +2,5 @@ document.addEventListener("partialsLoaded", () => {
     import("./header.js");
     import('./carousel-animated.js');
     import('./header-intro.js');
+    import('./main.section.hello-there.partial.js')
   });
