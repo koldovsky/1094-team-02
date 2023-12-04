4881893db7b3b6ab7d169aaebddfb17b74a38741
MaksymParashchuk <kaotic19@gmail.com>
2023-12-04 21:18:13 +0200

    ModalLogo

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 document.addEventListener("partialsLoaded", () => {
-    import("./header.js");
-    import("./who-i-am.js")
-    import('./carousel-animated.js');
-    import('./header-intro.js');
-    import('./main.section.hello-there.partial.js')
-    import('./logo-modal.js')
-  });
+  import("./header.js");
+  import("./who-i-am.js");
+  import('./carousel-animated.js');
+  import('./header-intro.js');
+  import('./main.section.hello-there.partial.js');
+  import("./logo-modal.js");
+});
--- js/logo-modal.js
+++ js/logo-modal.js
@@ -1,40 +1,54 @@
-// Open the Modal
-function openModal() {
-  document.getElementById("myModal").style.display = "block";
+let slideIndex = 1;
+
+function openModal(idx) {
+  document.getElementById("modalbox").style.display = "block";
+  showSlides(idx);
 }
 
-// Close the Modal
 function closeModal() {
-  document.getElementById("myModal").style.display = "none";
+  document.getElementById("modalbox").style.display = "none";
 }
 
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  slideIndex = n;
+  showSlides(slideIndex);
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
+  let slides = document.getElementsByClassName("modalslides");
+
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+
+  for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+
+  if (slides.length > 0) {
+    slides[slideIndex - 1].style.display = "block";
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
 }
+
+document.querySelector('.prev').onclick = function () {
+  plusSlides(-1);
+};
+
+document.querySelector('.next').onclick = function () {
+  plusSlides(1);
+};
+
+document.querySelector('.close').addEventListener('click', () => {
+  closeModal();
+});
+
+document.querySelectorAll('.column').forEach((img, idx) => {
+  img.addEventListener('click', () => {
+    openModal(idx);
+    currentSlide(idx + 1);
+  });
+});
+
