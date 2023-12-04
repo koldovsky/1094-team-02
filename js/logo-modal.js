5cd01de2e425d32ac4253b109028eb59e9248e5d
MaksymParashchuk <kaotic19@gmail.com>
2023-12-04 22:58:35 +0200

    Finalfixmodalbox

--- js/logo-modal.js
+++ js/logo-modal.js
@@ -21,8 +21,8 @@ function currentSlide(n) {
 function showSlides(n) {
   let slides = document.getElementsByClassName("modalslides");
 
-  if (n > slides.length) { slideIndex = 1 }
-  if (n < 1) { slideIndex = slides.length }
+  if (n > slides.length) { slideIndex = 1; }
+  if (n < 1) { slideIndex = slides.length; }
 
   for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
@@ -52,3 +52,35 @@ document.querySelectorAll('.column').forEach((img, idx) => {
   });
 });
 
+let currentZoom = 100; 
+const zoomStep = 10; 
+
+function zoomIn() {
+  currentZoom += zoomStep;
+  applyZoom();
+}
+
+function zoomOut() {
+  currentZoom -= zoomStep;
+  applyZoom();
+}
+
+function applyZoom() {
+  let modalContent = document.querySelector('.modal-content');
+  modalContent.style.transform = `translate(-50%, -50%) scale(${currentZoom / 100})`;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelectorAll('.zoom-controls button').forEach(function(button) {
+    button.addEventListener('click', function() {
+      if (this.classList.contains('zoom-in')) {
+        zoomIn();
+      } else if (this.classList.contains('zoom-out')) {
+        zoomOut();
+      }
+    });
+  });
+});
+
+window.zoomIn = zoomIn;
+window.zoomOut = zoomOut;
