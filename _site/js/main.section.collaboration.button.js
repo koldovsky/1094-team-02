9fc382f2d97f38ad03740706e6f998dcc21716ed
MaksJv <142892185+MaksJv@users.noreply.github.com>
2025-02-24 17:08:30 +0000

    error

--- _site/js/main.section.collaboration.button.js
+++
@@ -1,3 +0,0 @@
-btn.onclick = function() {
-    document.getElementById('footer__title').scrollIntoView({ behavior: 'smooth' });
-}
--- _site/js/portfolio.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("partialsLoaded", () => {
-  import("./portfolio.footer.js");
-  import("./portfolio-soc-project.js");
-  import("./portfolio.counter.js");
-  import('./portfolio-soc-project-text-about-wwf.partial.js');
-});
